870f348347b60652b7e23a9677d7775d44316602
Yuri <Cheremenskii.yuri@gmail.com>
2025-05-12 22:21:01 +0300

    это уже вторая задача, первая на предыдущей версии

--- main_alcm.py
+++ main_alcm.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.users_py import UserC
 from data.jobs_py import JobsC
+from forms.users_forms import RegisterForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'yandexlyceum_secret_key'
@@ -19,48 +20,41 @@ def tables_with_jobs():
     return render_template("tables_jobs.html", jobs=jobs)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(UserC).filter(UserC.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        try:
+            age = int(form.age.data)
+        except ValueError:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Возраст это int")
+        user = UserC(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            age=age,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 if __name__ == "__main__":
-    """db_session.global_init("db/alch1.db")
-    session = db_session.create_session()
-    user = UserC()
-    user.surname = "Qwerty"
-    user.name = "Amogus"
-    user.age = 32
-    user.position = "not captain"
-    user.speciality = "qqwerty"
-    user.address = "module_2"
-    user.email = "amogus@mars.org"
-    session.add(user)
-    session.commit()
-
-    job = JobsC()
-    job.team_leader = 2
-    job.job = 'qw'
-    job.work_size = 20
-    job.collaborators = '1, 3'
-    job.is_finished = False
-    session.add(job)
-    session.commit()
-
-    user = UserC()
-    user.surname = "y"
-    user.name = "Ay"
-    user.age = 32
-    user.position = "nin"
-    user.speciality = "qyy"
-    user.address = "module_3"
-    user.email = "s@mars.org"
-    session.add(user)
-    session.commit()
-
-    job = JobsC()
-    job.team_leader = 3
-    job.job = 'rty'
-    job.work_size = 5
-    job.collaborators = '1, 2'
-    job.is_finished = True
-    session.add(job)
-    session.commit()
-
-    session.close()"""
     main()
+
